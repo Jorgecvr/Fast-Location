5b51741b9773e2495e4f109f55f8f4b28ede7e63
jorgecvr <jorgealmeidacarvalho13@gmail.com>
2024-10-22 17:01:28 -0300

    Add Leitor QR nao esta lendo ainda

--- views/arearestrita/Edicao.js
+++ views/arearestrita/Edicao.js
@@ -1,15 +1,71 @@
 import React , {useEffect, useState} from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import MenuAreaRestrita from '../../assets/components/MenuAreaRestrita';
-
+import { Camera, CameraView } from 'expo-camera';
+import { css } from '../../assets/css/Css';
+import { TouchableOpacity } from 'react-native';
 
 export default function Edicao({navigation})
 {
-    
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [displayQR, setDisplayQR] = useState('flex');
+    const [displayForm, setDisplayForm] = useState('none');
+    const [code, setCode] = useState(null);
+    const [product, setProduct] = useState(null);
+    const [localization, setLocalization] = useState(null);
+
+    useEffect(()=>{
+        (async () =>{
+            const {status} = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    },[]);
+
+    async function handleBarCodeScanned ({ type, data }){
+        setScanned(true);
+        setDisplayQR('none');
+        setDisplayQR('none');
+        setDisplayForm('flex');
+        setCode({data});
+    };
+
+    async function sendForm() {
+        
+    }
+
     return(
         <View>
              <MenuAreaRestrita title='Edição' navigation={navigation} />
+        
+            <CameraView
+                onBarCodeScanned={scanned ? undefined : value=>handleBarCodeScanned(value)}
+                style={css.qr__code(displayQR)}
+            />
+
+            <View style={css.qr__form(displayForm)}>
+                <Text>Código do Produto: {code}</Text>
+                <View style={css.login__input}>
+                    <TextInput 
+                        placeholder='Nome do Produto:'
+                        onChangeText={text=>setProduct(text)}
+                        value={product}
+                    />
+                </View>
+
+                <View style={css.login__input}>
+                    <TextInput 
+                        placeholder='Localização do Produto:'
+                        onChangeText={text=>setLocalization(text)}
+                        value={setLocalization}
+                    />
+                </View>
+
+                <TouchableOpacity style={css.login__button} onPress={()=>sendForm()}>
+                     <Text>Atualizar</Text>
+                </TouchableOpacity>
+            </View> 
         </View>
     );
 }
